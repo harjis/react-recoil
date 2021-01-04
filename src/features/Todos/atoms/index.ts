3d0139518b96e37ca3edf22c0262ce9bf6ebd082
Joonas Harjukallio <harjukallio@gmail.com>
2021-01-04 17:35:34 +0200

    Remove not needed

--- src/features/Todos/atoms/index.ts
+++ src/features/Todos/atoms/index.ts
@@ -7,23 +7,9 @@ export const todoIdsState = atom<number[]>({
   default: [],
 });
 
-const tempTodo = atomFamily<Todo | null, number>({
-  key: "todo",
-  default: null,
-});
 export const todoState = atomFamily<Todo, number>({
-  key: "todoState",
-  default: selectorFamily<Todo, number>({
-    key: "todoState/default",
-    get: (todoId) => ({ get }) => {
-      const todo = get(tempTodo(todoId));
-      if (todo === null) {
-        throw new Error(`Fatal error: Todo with id ${todoId} was not found`);
-      }
-      return todo;
-    },
-    set: (todoId) => ({ set }, newTodo) => set(tempTodo(todoId), newTodo),
-  }),
+  key: "todo",
+  default: {} as Todo,
 });
 
 export const todoListFilterState = atom({
